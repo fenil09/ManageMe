524defc6c1763cf84f7884b15d2c821180abcf93
wallace08797 <fenilahir16@gmail.com>
2022-09-15 16:20:29 +0530

    Starting Screen updated with lagging issues in viewflipper getting fixed.

--- app/src/main/java/com/task/manageme/MainActivity.kt
+++ app/src/main/java/com/task/manageme/MainActivity.kt
@@ -2,11 +2,14 @@ package com.task.manageme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ViewFlipper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
         supportActionBar?.hide()
     }
 }
